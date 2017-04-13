91986dfdab3c04151713e6a651ff6ed1876adcbc
Michel Gerlach <daexel@DESKTOP-TU1V7SB.fritz.box>
2017-04-13 19:41:13 +0200

    erster Commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: daexel
+ *
+ * Created on 13. April 2017, 19:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
